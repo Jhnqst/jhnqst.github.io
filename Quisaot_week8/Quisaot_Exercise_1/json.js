9a4636ba2830cdf00fe2c8b96a85e0bcb4cb94cd
Jhnqst <johnquisaot@gmail.com>
2026-07-03 14:28:02 +0800

    Delete Quisaot_week8 directory

--- Quisaot_week8/Quisaot_Exercise_1/json.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('changeTextBtn').addEventListener('click', () => {
-    document.getElementById('header').textContent = 'You Clicked the Button!';
-    document.getElementById('paragraph').textContent = 'Great! The text has changed successfully.';
-});
